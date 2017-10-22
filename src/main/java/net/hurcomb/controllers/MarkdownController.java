b6e26cd6d02eb7ba2032aba96dd6570fddf72402
captaincaaaaveman <david@hurcomb.net>
2017-10-22 16:41:28 +0100

    Cross origin

--- src/main/java/net/hurcomb/controllers/MarkdownController.java
+++ src/main/java/net/hurcomb/controllers/MarkdownController.java
@@ -5,11 +5,13 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import net.hurcomb.domain.Markdown;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class MarkdownController {
 
     private static final String template = "# Markdown";
