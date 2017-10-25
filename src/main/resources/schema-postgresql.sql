5fae13e25390782789ed1c01a420b78524da37bc
captaincaaaaveman <david@hurcomb.net>
2017-10-25 20:36:10 +0100

    database

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,7 +1,7 @@
---CREATE TABLE IF NOT EXISTS blog (
---   id INT NOT NULL,
---   markdown VARCHAR(6000) NOT NULL,
---   tags VARCHAR(100) NOT NULL,
---   PRIMARY KEY (id) 
---);
+CREATE TABLE IF NOT EXISTS blog (
+   id INT NOT NULL,
+   markdown VARCHAR(6000) NOT NULL,
+   tags VARCHAR(100) NOT NULL,
+   PRIMARY KEY (id) 
+);
 
