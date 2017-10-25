ddb274ce6a42656b8c23803345102309b52d4713
captaincaaaaveman <david@hurcomb.net>
2017-10-25 20:39:07 +0100

    database

--- src/main/resources/data-postgresql.sql
+++
@@ -1,1 +0,0 @@
-insert into blog values (2,'# Database Postgres Blog','Java,SpringBoot');
--- src/main/resources/schema-postgresql.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS blog (
-   id INT NOT NULL,
-   markdown VARCHAR(6000) NOT NULL,
-   tags VARCHAR(100) NOT NULL,
-   PRIMARY KEY (id) 
-);
-
