2fc513a96ed2fafa8f0cd08b1553425b2d06ed4f
captaincaaaaveman <david@hurcomb.net>
2017-10-25 20:34:02 +0100

    database

--- src/main/java/net/hurcomb/Application.java
+++ src/main/java/net/hurcomb/Application.java
@@ -1,6 +1,7 @@
 package net.hurcomb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
 	@Override
